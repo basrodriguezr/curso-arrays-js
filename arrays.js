2fbb1eb09f6b244305b535c80efa5991f7f00968
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-13 21:35:46 -0400

    recorrer arrays numericos

---
+++ arrays.js
@@ -0,0 +1,24 @@
+/*
+    curso de arrays 
+*/
+
+let numeros = [24,500,0,3]
+
+//recorrer numeros secuencialmente
+for(let i=0;i<numeros.length;i++){
+    //console.log(numeros[i]);
+}
+
+//recorrer numeros secuencialmente desde el final
+for(let i=numeros.length-1;i>=0;i--){
+    console.log(numeros[i]);
+}
+
+//funcion para obtener una posicion dentro del array "at()"
+//console.log(numeros.at(2)); //da como resultado la posición 2 del array, 0 en este caso
+//console.log(numeros.at(numeros.length-1)); //obtener la ultima posicion del array
+
+
+
+
+
