ee689670bd8573498c109a761997cb8d99e2a05e
bastian rodríguez <basrodriguezr@gmail.com>
2022-06-13 21:41:14 -0400

    se agregan las funcionalidades a funciones

--- arrays.js
+++ arrays.js
@@ -4,19 +4,29 @@
 
 let numeros = [24,500,0,3]
 
+/*
+    funcion para obtener una posicion dentro del array "at()"
+    console.log(numeros.at(2));  da como resultado la posición 2 del array, 0 en este caso
+    console.log(numeros.at(numeros.length-1)); obtener la ultima posicion del array
+*/
+
 //recorrer numeros secuencialmente
-for(let i=0;i<numeros.length;i++){
-    //console.log(numeros[i]);
+showArrayAsc = (array) =>{
+    for(let i=0;i<array.length;i++){
+        console.log(array[i]);
+    }
 }
 
 //recorrer numeros secuencialmente desde el final
-for(let i=numeros.length-1;i>=0;i--){
-    console.log(numeros[i]);
+showArrayDesc = (array) => {
+    for(let i=array.length-1;i>=0;i--){
+        console.log(array[i]);
+    }
 }
 
-//funcion para obtener una posicion dentro del array "at()"
-//console.log(numeros.at(2)); //da como resultado la posición 2 del array, 0 en este caso
-//console.log(numeros.at(numeros.length-1)); //obtener la ultima posicion del array
+// showArrayAsc(numeros);
+// showArrayDesc(numeros);
+
 
 
 
